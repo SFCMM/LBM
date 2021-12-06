5f453549b71b13936371e4a269dd564742e4c19d
svchb <berger.sven@gmail.com>
2021-12-06 12:18:39 +0100

    fix deactivated stuff for debug

--- src/lbm/lbm_solver.cpp
+++ src/lbm/lbm_solver.cpp
@@ -237,12 +237,8 @@ void LBMSolver<DEBUG_LEVEL, LBTYPE, EQ>::timeStep() {
   executePostprocess(pp::HOOK::BEFORETIMESTEP);
   currToOldVars();
   updateMacroscopicValues();
-  //  calcEquilibriumMoments();
-  //  collisionStep();
-  //  forcing();
-  //  prePropBoundaryCnd();
-  //  propagationStep();
-  //  boundaryCnd();
+
+  
   executePostprocess(pp::HOOK::AFTERTIMESTEP);
   RECORD_TIMER_STOP(TimeKeeper[Timers::LBMCalc]);
 }
