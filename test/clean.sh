a4420516d0953930a226c8b92bd652a2b719d90d
svchb <berger.sven@gmail.com>
2021-12-16 02:08:34 +0100

    add script to run testcases

---
+++ test/clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+find . -name "*.vtp" -type f -delete
+find . -name "*_log" -type f -delete
+find . -name "*.csv" -type f -delete
---
+++ test/run.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+BINPATH=""
+SUCCESS=0
+FAILED=0
+
+if [[ -z "${SFCMM_LBM_BIN}" ]]; then
+  echo "Environment variable SFCMM_LBM_BIN is not set!"
+  exit
+else
+  BINPATH="${SFCMM_LBM_BIN}"
+fi
+
+DEFAULT_OMP_THREADS=4
+LOG_PATH="run_log_"$(date +%s)
+
+run_test(){
+  #todo:redirect output output logpath
+  if OMP_NUM_THREADS=$DEFAULT_OMP_THREADS $BINPATH "$1"; then
+    SUCCESS=$SUCCESS+1
+  else
+    echo "Test case failed"
+    FAILED=$FAILED+1
+  fi
+}
+
+#todo: copy log files to log path
+#mkdir $LOG_PATH
+
+./clean.sh
+cd couette || exit
+run_test couette.json
+run_test couette_bnd.json
+cd ..
+
+cd poiseuille || exit
+run_test poiseuille.json
+cd ..
+
+cd poisson || exit
+run_test poisson1D.json
+run_test poisson2D.json
+cd ..
+
+./clean.sh
+
+if [ $FAILED == 0 ]; then
+  echo "All testcases have been successful"
+  exit 0
+else
+  exit 1
+fi
