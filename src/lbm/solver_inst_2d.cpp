35f5658664c4e199e1639b874386efbd831352f9
svchb <berger.sven@gmail.com>
2022-08-12 14:00:14 +0200

    cleanup

--- src/lbm/solver_inst_2d.cpp
+++ src/lbm/solver_inst_2d.cpp
@@ -1,27 +1,11 @@
 #include "solver.cpp"
 
-// todo: probably not needed
-// extern template class LBMSolver<Debug_Level::no_debug, LBMethodType::D2Q5, LBEquation::Navier_Stokes>;
-// template class LBMSolver<Debug_Level::no_debug, LBMethodType::D2Q5, LBEquation::Navier_Stokes>;
-// extern template class LBMSolver<Debug_Level::debug, LBMethodType::D2Q5, LBEquation::Navier_Stokes>;
-// template class LBMSolver<Debug_Level::debug, LBMethodType::D2Q5, LBEquation::Navier_Stokes>;
-// extern template class LBMSolver<Debug_Level::max_debug, LBMethodType::D2Q5, LBEquation::Navier_Stokes>;
-// template class LBMSolver<Debug_Level::max_debug, LBMethodType::D2Q5, LBEquation::Navier_Stokes>;
-
 extern template class LBMSolver<Debug_Level::no_debug, LBMethodType::D2Q5, LBEquationType::Poisson>;
 template class LBMSolver<Debug_Level::no_debug, LBMethodType::D2Q5, LBEquationType::Poisson>;
 extern template class LBMSolver<Debug_Level::debug, LBMethodType::D2Q5, LBEquationType::Poisson>;
 template class LBMSolver<Debug_Level::debug, LBMethodType::D2Q5, LBEquationType::Poisson>;
 extern template class LBMSolver<Debug_Level::max_debug, LBMethodType::D2Q5, LBEquationType::Poisson>;
 template class LBMSolver<Debug_Level::max_debug, LBMethodType::D2Q5, LBEquationType::Poisson>;
-
-// todo: probably not needed
-// extern template class LBMSolver<Debug_Level::no_debug, LBMethodType::D2Q5, LBEquation::Navier_Stokes_Poisson>;
-// template class LBMSolver<Debug_Level::no_debug, LBMethodType::D2Q5, LBEquation::Navier_Stokes_Poisson>;
-// extern template class LBMSolver<Debug_Level::debug, LBMethodType::D2Q5, LBEquation::Navier_Stokes_Poisson>;
-// template class LBMSolver<Debug_Level::debug, LBMethodType::D2Q5, LBEquation::Navier_Stokes_Poisson>;
-// extern template class LBMSolver<Debug_Level::max_debug, LBMethodType::D2Q5, LBEquation::Navier_Stokes_Poisson>;
-// template class LBMSolver<Debug_Level::max_debug, LBMethodType::D2Q5, LBEquation::Navier_Stokes_Poisson>;
 
 extern template class LBMSolver<Debug_Level::no_debug, LBMethodType::D2Q9, LBEquationType::Navier_Stokes>;
 template class LBMSolver<Debug_Level::no_debug, LBMethodType::D2Q9, LBEquationType::Navier_Stokes>;
